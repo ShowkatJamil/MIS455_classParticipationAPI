3c98254f19ed8658255256e4983ffefb8d1f472e
ShowkatJamil <showkatjamil2012@gmail.com>
2023-12-05 04:42:14 +0600

    mealdB

---
+++ mealDb.js
@@ -0,0 +1,40 @@
+
+function connect(){
+
+ var searchTerm = document.getElementById("searchBox").value ; 
+
+ var url=`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchTerm}`;
+
+
+ console.log (url);
+
+ fetch (url)
+ .then (res => res.json())
+ .then (data => display(data))
+
+}
+
+function display (data){
+
+  //console.log (data);
+  var allMeals = data.meals ;
+  console.log(allMeals);
+
+  var oldContent = document.getElementById("container");
+  oldContent.textContent = ""; 
+
+  for (var i=0; i< Math.min(5 , allMeals.length); i++){
+
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = `<div class= "mealName"> Meal Name: ${allMeals[i].strMeal} </div>  <br>
+                        Meal ID: ${allMeals[i].idMeal}<br>
+                        <img src="${allMeals[i].strMealThumb}"> <br> <br>
+                         <br>
+                        ` 
+  
+
+     newDiv.classList.add("mealStyle");
+     oldContent.appendChild(newDiv);
+  }
+
+}
